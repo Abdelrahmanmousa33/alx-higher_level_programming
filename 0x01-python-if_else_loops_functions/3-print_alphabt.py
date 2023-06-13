0cd4e06d390636881011143f55d5c7d3ec248f51
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-06-13 03:06:55 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+for alpha in range(97, 123):
+    if chr(alpha) == 'e' or chr(alpha) == 'q':
+        continue
+    print("{}".format(chr(alpha)), end="")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,5 @@
+for number in range(0, 100):
+    if number == 99:
+        print("99")
+    else:
+        print("{:02}".format(number), end=", ")
