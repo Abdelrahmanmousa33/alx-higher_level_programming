72c6e96377b2513020eaabe28741084363adeac5
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-06-27 11:28:53 -0700

    task 1, and 2

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""""Define a square class."""
+class Square:
+    """Square class."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+class Square:
+    """square class"""
+    def __init__(self, size):
+        self.__size = size
