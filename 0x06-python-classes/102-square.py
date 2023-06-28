7224b7e6000c2569b99ccf3e4e77c15307f02458
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-06-27 17:54:09 -0700

    Typo fixed

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -15,7 +15,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value) != int or type(value) != float:
+        if type(value) != int and type(value) != float:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
