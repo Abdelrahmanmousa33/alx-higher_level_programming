c92b84b52e46ea06adb3eb0b2f71443023ab7b25
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-07-17 13:43:28 -0700

    task 18 and 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,8 +28,34 @@ class Base:
         """writes the JSON string representation of list_objs to a file."""
         filename = cls.__name__ + ".json"
         with open(filename, "w") as f:
-            if list_objs == None:
+            if list_objs is None:
                 f.write("[]")
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string."""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary"""
+        if dictionary != {}:
+            new = cls(1, 1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as f:
+                list_dicts = Base.from_json_string(f.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
