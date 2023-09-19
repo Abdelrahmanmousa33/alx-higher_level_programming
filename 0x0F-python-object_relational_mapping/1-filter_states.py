04933f96e4864c5c47ceeac7fbbbd6f2f4d3af7a
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-09-18 23:57:46 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Scribt that lists all states from a database"""
+
+import MySQLdb
+import sys
+
+if (len(sys.argv) == 4):
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    data = cur.fetchall()
+    for row in data:
+        if (row[1][0] == "N"):
+            print(row)
+    db.close()
+
+
+if "__name__" == "__main__":
+    main()
