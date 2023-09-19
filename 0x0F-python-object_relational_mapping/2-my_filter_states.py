666d99ea5e30f9419fe243d24c799b21bfb56270
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-09-19 00:12:21 -0700

    task 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,6 @@
 Scribt that lists all states from a database
 where name maches the argument
 """
-
 import MySQLdb
 import sys
 
@@ -11,11 +10,10 @@ if (len(sys.argv) == 5):
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY `name` = {}".format(sys.argv[4]))
     data = cur.fetchall()
     for row in data:
-        if (row[1] == sys.argv[4]):
-            print(row)
+        print(row)
     db.close()
 
 
