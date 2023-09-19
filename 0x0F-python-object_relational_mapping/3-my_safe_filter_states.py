04f3e57bd52a493a9ab34867cbb90fdde6995b86
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-09-19 00:53:36 -0700

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Scribt that lists all states from a database
-where name maches the argument
+where name maches the argument and prevent sql injection
 """
 import MySQLdb
 import sys
@@ -10,7 +10,8 @@ if (len(sys.argv) == 5):
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY `name` = %s",(sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE BINARY `name` = %s",
+                (sys.argv[4], ))
     data = cur.fetchall()
     for row in data:
         print(row)
