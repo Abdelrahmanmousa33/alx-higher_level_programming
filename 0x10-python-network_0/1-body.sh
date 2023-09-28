81a3ba83d9fd4721e96ea7f59047de53242f0853
abdelrahmanmousa33 <abdelrahmanmousa33@gmail.com>
2023-09-28 11:56:54 -0700

    made file 3 lines long

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL,
-#displays the body of the response of 200 status code.
+# Takes in a URL, sends a GET request to the URL, displays the body of the response of 200 status code.
 curl -s "$1" -L
