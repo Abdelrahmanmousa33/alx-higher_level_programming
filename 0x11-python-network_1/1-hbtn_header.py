affe2315cd3ce1d33873a7aa053d831ac6fe05bc
abdelrahmanmousa33 <abdelrahmanmousa33@gmail.com>
2023-09-29 09:30:56 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@
 import urllib.request
 import sys
 
-if __name__==__main__:
+if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(dict(response.info()).get("X-Request-Id"))
