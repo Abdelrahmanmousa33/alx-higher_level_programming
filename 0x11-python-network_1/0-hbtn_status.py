ba1b8bed32e1a517a4a6084b7e57ae4a83a25e41
abdelrahmanmousa33 <abdelrahmanmousa33@gmail.com>
2023-09-29 09:14:27 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# script that fetches https://alx-intranet.hbtn.io/status
+"""script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
+
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
