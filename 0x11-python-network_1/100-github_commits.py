dc5a0e326a0ac646ab99d23407b6efb021ebcfa5
abdelrahmanmousa33 <abdelrahmanmousa33@gmail.com>
2023-09-29 14:50:21 -0700

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
+                                                              sys.argv[1])
+    r = requests.get(url)
+    r = r.json()
+    for i in range(10):
+        print("{}: {}".format(r[i].get('sha'),
+              r[i].get('commit').get('author').get('name')))
